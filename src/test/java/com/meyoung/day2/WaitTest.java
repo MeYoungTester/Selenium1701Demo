08910953e4624746abfea0069a29fd772e069781
MeYoung <275441330@qq.com>
2017-06-15 19:07:13 +0800

    update

--- src/test/java/com/meyoung/day2/WaitTest.java
+++ src/test/java/com/meyoung/day2/WaitTest.java
@@ -47,8 +47,6 @@ public class WaitTest {
         Assert.assertEquals(text, "wait for display");
     }
 
-
-
     @Test
     public void waitTest2() throws InterruptedException {
         driver.get("file:///C:/selenium_html/index.html");
